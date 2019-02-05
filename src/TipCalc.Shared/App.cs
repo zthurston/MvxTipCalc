5413bcc180f621960317a7f355d8f83f547ab0d6
Zach Thurston <zach@zthurston.com>
2019-02-04 19:29:50 -0700

    Add first ViewModel and App entry point

---
+++ src/TipCalc.Shared/App.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MvvmCross;
+using MvvmCross.ViewModels;
+using TipCalc.Shared.Services;
+using TipCalc.Shared.ViewModels;
+
+namespace TipCalc.Shared
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            Mvx.IoCProvider.RegisterType<ICalculationService, CalculationService>();
+
+            RegisterAppStart<TipViewModel>();
+        }
+    }
+}
---
+++ src/TipCalc.Shared/ViewModels/TipViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading.Tasks;
+using MvvmCross.ViewModels;
+using TipCalc.Shared.Services;
+
+namespace TipCalc.Shared.ViewModels
+{
+    public class TipViewModel : MvxViewModel
+    {
+        private readonly ICalculationService _calculationService;
+
+        public TipViewModel(ICalculationService calculationService)
+        {
+            _calculationService = calculationService;
+        }
+
+        public override async Task Initialize()
+        {
+            await base.Initialize();
+            _subTotal = 100;
+            _generosity = 18;
+
+            Recalculate();
+        }
+
+        private int _generosity;
+        public int Generosity
+        {
+            get => _generosity;
+            set
+            {
+                _generosity = value;
+                RaisePropertyChanged(nameof(Generosity));
+
+                Recalculate();
+            }
+        }
+
+        private decimal _subTotal;
+        public decimal SubTotal
+        {
+            get => _subTotal;
+            set
+            {
+                _subTotal = value;
+                RaisePropertyChanged(nameof(SubTotal));
+
+                Recalculate();
+            }
+        }
+
+        private decimal _tip;
+        public decimal Tip
+        {
+            get => _tip;
+            set
+            {
+                _tip = value;
+                RaisePropertyChanged(nameof(Tip));
+            }
+        }
+
+        private void Recalculate()
+        {
+            Tip = _calculationService.TipAmount(SubTotal, Generosity);
+        }
+    }
+}
