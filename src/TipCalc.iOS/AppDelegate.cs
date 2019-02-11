44b0317f3abc01aa25edb88b2ee8afd1111bde55
Zach Thurston <zach@zthurston.com>
2019-02-10 20:00:03 -0700

    Add iOS view implementation

---
+++ src/TipCalc.iOS/AppDelegate.cs
@@ -0,0 +1,19 @@
+﻿using Foundation;
+using MvvmCross.Platforms.Ios.Core;
+using TipCalc.Shared;
+using UIKit;
+
+namespace TipCalc.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register(nameof(AppDelegate))]
+    public class AppDelegate : MvxApplicationDelegate<MvxIosSetup<App>, App>
+    {
+        public override void FinishedLaunching(UIApplication application)
+        {
+            base.FinishedLaunching(application);
+        }
+    }
+}
+
---
+++ src/TipCalc.iOS/Views/TipView.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.Platforms.Ios.Views;
+using TipCalc.Shared.ViewModels;
+using UIKit;
+
+namespace TipCalc.iOS.Views
+{
+    public partial class TipView : MvxViewController<TipViewModel>
+    {
+        public TipView() : base(nameof(TipView), null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            var bindingSet = this.CreateBindingSet<TipView, TipViewModel>();
+            bindingSet.Bind(TipLabel).To(vm => vm.Tip);
+            bindingSet.Bind(SubTotalTextField).To(vm => vm.SubTotal);
+            bindingSet.Bind(GenerositySlider).To(vm => vm.Generosity);
+            bindingSet.Bind(GenerosityTextField).To(vm => vm.Generosity);
+            bindingSet.Apply();
+
+            // close keyboard when the screen is tapped outside 
+            // the bounds of the SubTotalTextField
+            View.AddGestureRecognizer(new UITapGestureRecognizer(() =>
+            {
+                SubTotalTextField.ResignFirstResponder();
+            }));
+        }
+    }
+}
+
