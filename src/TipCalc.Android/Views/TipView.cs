0100292abf225b2c951dbc7de3a7eb76db1edfd0
Zach Thurston <zach@zthurston.com>
2019-02-10 18:31:22 -0700

    Add Android UI implementation

---
+++ src/TipCalc.Android/Views/TipView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using MvvmCross.Platforms.Android.Views;
+using TipCalc.Shared.ViewModels;
+
+namespace TipCalc.Android.Views
+{
+    [Activity(Label = "Tip Calculator", MainLauncher = true)]
+    public class TipView : MvxActivity<TipViewModel>
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.TipView);
+        }
+    }
+}
